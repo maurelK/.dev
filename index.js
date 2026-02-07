c83adf5063d04248cef6c89302dea6ccae906f6e
maurel <maurel.kouassi@epitech.eu>
2026-02-07 13:16:31 +0100

    feat:We alredy initiate the repository and partially done the work

---
+++ index.js
@@ -0,0 +1,15 @@
+function copyEmail() {
+            navigator.clipboard.writeText('jose.maurel@epitech.eu').then(function() {
+                alert('Email copied to clipboard!');
+            });
+        }
+
+        // Simple typing animation
+        const dynamicText = document.querySelector('.dynamic-text');
+        const roles = ['Computer Science Student', 'Software Developer', 'Full-Stack Developer', 'Network Enthusiast'];
+        let roleIndex = 0;
+        
+        setInterval(() => {
+            dynamicText.textContent = roles[roleIndex];
+            roleIndex = (roleIndex + 1) % roles.length;
+        }, 3000);
